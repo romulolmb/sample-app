e929176b31d27df04cbd5d7c4fe0185fdc60a6ea
Rômulo Brito <romulo.brito@visagio.com>
2018-09-30 17:53:32 -0300

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,10 @@ Rails.application.routes.draw do
   get 'contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
-  
-  
+
   get 'signup', to: 'users#new'
-  
-  
+  post 'signup', to: 'users#create'
+  resources :users
+
+
 end
