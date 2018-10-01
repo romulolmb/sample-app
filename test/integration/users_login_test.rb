df1226e2e8a2299b005c97a0311d57f06d67582c
Rômulo Brito <romulo.brito@visagio.com>
2018-10-01 10:24:22 -0300

    Implement advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,6 +6,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:romulo)
   end
 
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '1')
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
+  end
+
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
@@ -30,6 +41,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
